ab4d42ad7adaccbb61e042452216c8f2a63b4522
Jeffrey Estrada <jestrada@wpi.edu>
2018-03-28 09:43:05 -0400

    Added rpi bomb

--- code/lil_bomb.c
+++ code/lil_bomb.c
@@ -17,9 +17,7 @@ void phase1(){
     printf("You'll never know how to defuse this bomb!\n");
     printf("You'd be a fool if you thought you could simply defuse it!\n");
     
-    char* a = "Password";
-    char* b = "distraction";
-    char* c = "other distraction";
+    char* a = "Roses are red, violets are blue, ACM is the best and so are you!";
     char* buffer = (char*) malloc (8);
     gets(buffer);
     if (!strcmp(a, buffer)){
@@ -40,7 +38,9 @@ void phase2(){
 	if (!a){
 		explode();
 	}
+	printf("Why are you so good at defusing my phases\n");
 }
+
 void explode(){
 	printf("YOU FOOL, YOU'VE RUINED EVERYTHING AND I WILL NOW RUIN YOUR GRADE!!\n");
 	printf("HOW WOULD A ZERO MAKE YOU FEEL??\n");
